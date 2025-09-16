98b21f5a3ef2bc038423b068ce656dc43e2f84ec
petsereypanha <sereypanhapet@gmail.com>
2025-09-16 21:44:11 +0700

    loops fundamentals

--- fundamentals/src/main.rs
+++ fundamentals/src/main.rs
@@ -2,10 +2,12 @@ mod data_type;
 mod if_else_if;
 mod if_else;
 mod functions;
+mod loops;
 
 fn main() {
     data_type::data_type();
     if_else_if::if_else_if();
     if_else::if_else();
     functions::functions();
+    loops::loops();
 }
