b8f25ab2a808912b275596c33a952fed78c47277
petsereypanha <sereypanhapet@gmail.com>
2025-09-16 21:41:53 +0700

    functions fundamentals

---
+++ fundamentals/src/functions.rs
@@ -0,0 +1,21 @@
+pub(crate) fn functions() {
+    first_func();
+    second_func(1, 3);
+
+    let sum = sum(10, 20);
+    println!("Sum of 10 and 20 = {}", sum);
+}
+
+// first_func: No parameters
+fn first_func(){
+    println!("first_func called...");
+}
+
+// second_func: two parameters of type i32 and u8
+fn second_func(x : i32, y : u8){
+    println!("second_func called... {}, {}", x, y);
+}
+
+fn sum(a : i32, b : i32) -> i32 {
+    return a + b;
+}
--- fundamentals/src/main.rs
+++ fundamentals/src/main.rs
@@ -1,9 +1,11 @@
 mod data_type;
 mod if_else_if;
 mod if_else;
+mod functions;
 
 fn main() {
     data_type::data_type();
     if_else_if::if_else_if();
     if_else::if_else();
+    functions::functions();
 }
