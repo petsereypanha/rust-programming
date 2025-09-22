76c190cc48e1cb145b4e319aa5a8241956177432
petsereypanha <sereypanhapet@gmail.com>
2025-09-22 21:48:40 +0700

    vector fundamental

---
+++ fundamentals/src/vector.rs
@@ -0,0 +1,15 @@
+pub fn vector() {
+    let mut v1: Vec<u8> = Vec::new();
+    let mut v2 = vec![1.5, 2.5, 3.5];
+    println!("v1 = {:?}, v2 = {:?}", v1, v2);
+
+    v1.push(10);
+    v1.push(20);
+    v1.push(100);
+    v2.pop();
+    println!("v1 = {:?}, v2 = {:?}", v1, v2);
+
+    println!("Second element = {}", v2[1]);
+    println!("Second element = {:?}", v2.get(1));
+    println!("Fourth element = {:?}", v2.get(3));
+}
